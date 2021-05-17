cb2a6d32527887bfb746939291af8604e81d05b8
Mohamed_Adel_ <moh95_1@yahoo.com>
2021-05-17 11:49:35 +0200

    Initialize the project

---
+++ src/main/java/com/jumia/contactor/ContactorApplication.java
@@ -0,0 +1,13 @@
+package com.jumia.contactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jumia/contactor/ContactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumia.contactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
